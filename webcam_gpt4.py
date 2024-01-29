eb7ea74d7acb0fd70ac68c40750703fe1115c58e
dlskd255 <einstein1915@naver.com>
2024-01-29 20:31:50 +0900

    Update webcam_gpt4.py

--- webcam_gpt4.py
+++ webcam_gpt4.py
@@ -8,7 +8,8 @@ from multiprocessing import Process, Manager
 import multiprocessing
 import math
 import time
-
+import traceback
+import asyncio
 import os
 import sys
 import urllib.request
@@ -24,9 +25,15 @@ def play_mp3(mp3_file):
     pygame.mixer.music.play()
 
 mp3_file = "./son.mp3"
-"""mp_drawing = mp.solutions.drawing_utils
-mp_pose = mp.solutions.pose"""
 
+"""
+# 관측 시야(Field Of View) - 최대 120 기본값 90
+fov = "120"
+# 방향 - 범위 0 ~ 360 (0 or 360::북, 180: 남)
+heading = "-45"
+# 카메라 상하 방향 설정 - 범위 -90 ~ 90 기본값 0
+pitch = "30"
+"""
 
 def calculate_angle(a, b, c):
     """세 점 간의 각도를 계산하는 함수"""
@@ -37,18 +44,20 @@ def calculate_angle(a, b, c):
 # Pose 모델 로드
 #pose = mp_pose.Pose()
 
-def head_nod_algorithm(left_shoulder_coords, right_shoulder_coords, head_coords):
+def head_nod_algorithm(left_ear,right_ear):#(left_shoulder_coords, right_shoulder_coords, head_coords):
     """고개를 좌우로 흔드는 경우를 판단하는 알고리즘"""
-    shoulder_center_x = (left_shoulder_coords[0] + right_shoulder_coords[0]) // 2
-
+   
     # 머리의 좌우 이동량 계산
-    head_horizontal_movement = head_coords[0] - shoulder_center_x
-    print(head_horizontal_movement)
-    if abs(head_horizontal_movement) > 50:  # 예제의 임계값
-        return True
+    head_horizontal_movement = (left_ear.z - right_ear.z) 
+    horiDiff = cv2.norm((left_ear.x,left_ear.y,left_ear.z),(right_ear.x,right_ear.y,right_ear.z))
+    head_horizontal_movement /= horiDiff
+    #print(head_horizontal_movement)
+    if head_horizontal_movement > 0.3:  # 예제의 임계값
+        return [-1,head_horizontal_movement]
+    elif head_horizontal_movement <-0.3:
+        return [1,head_horizontal_movement]
     else:
-        return False
-
+        return [0,head_horizontal_movement]
 
 def speech_recognator(firstLocation,condition):
     api_key = "AIzaSyC1Ax9tlpuKI8qC7hh0RVnUNkJYrAOJe10"
@@ -155,19 +164,20 @@ def update_street_view(location, pitchheading,Picture):
     while firstTime == True:        
         try:            
             #print(location,pitchheading)
+            print(pitchheading)
             street_view_image = get_street_view_image(location, pitchheading)#(location, pitchheading)
             if street_view_image is not None:
                 street_view_image_prev = street_view_image
-                print("good?")
+                #print("good?")
 
             #print(street_view_image[5])
             if street_view_image is not None:
                 cv2.imshow('Street View', street_view_image) 
                 
             if street_view_image_prev is not None:
-                print("recived :",Picture)
+                #print("recived :",Picture)
                 if Picture.value > 0:
-                    print("go")
+                    #print("go")
                     cv2.imwrite(pathFolder+f"{pictureIndex}.jpg",street_view_image_prev)
                     pictureIndex += 1
                     Picture.value = 0
@@ -184,85 +194,96 @@ def webcam_pose_estimation(PitchHeading,sharedPicture):
     pose = mp_pose.Pose()  # Initialize once
     mp_drawing = mp.solutions.drawing_utils
     mp_hands = mp.solutions.hands
+    phLocal = [0,0]
     with mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5) as hands:
         last_capture_time = time.time()  # 마지막 촬영 시간 초기화
         capture_interval = 4  # 촬영 간격 (초)
 
         while cap.isOpened():
-            ret, frame = cap.read()
-            if not ret:
-                continue
-            # BGR을 RGB로 변환
-            rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            try:
+                ret, frame = cap.read()
+                if not ret:
+                    continue
+                # BGR을 RGB로 변환
+                rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-            # 손 감지 수행
-            resultsHand = hands.process(rgb_frame)
-            # 손 키포인트를 그리기 위한 코드
-            if resultsHand.multi_hand_landmarks:
-                for landmarks in resultsHand.multi_hand_landmarks:
-                    mp_drawing.draw_landmarks(frame, landmarks, mp_hands.HAND_CONNECTIONS)
+                results = pose.process(frame)
 
-                    # 검지와 엄지 각도 계산
-                    index_finger = landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
-                    thumb = landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP]
-                    indexMCP = landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_MCP]
-                    distance = cv2.norm(
-                        (index_finger.x, index_finger.y,index_finger.z),
-                        (thumb.x, thumb.y,thumb.z)
-                    )
-                    distance1 =cv2.norm(
-                        (index_finger.x, index_finger.y,index_finger.z),
-                        (indexMCP.x,indexMCP.y,indexMCP.z)
-                    )
+                if results.pose_landmarks:
+                    # 어깨(landmark 12, 11)와 머리(landmark 30)의 좌표 획득
+                    left_shoulder = results.pose_landmarks.landmark[12]
+                    right_shoulder = results.pose_landmarks.landmark[11]
+                    head = results.pose_landmarks.landmark[30]
+                    left_ear = results.pose_landmarks.landmark[2]
+                    right_ear = results.pose_landmarks.landmark[5]
+                    shoulder_distance1 = cv2.norm((left_shoulder.x,left_shoulder.y,left_shoulder.z),(right_shoulder.x,right_shoulder.y,right_shoulder.z))
+                    left_thumb1 = results.pose_landmarks.landmark[21]
+                    right_index1 = results.pose_landmarks.landmark[22]
+                    finger_distance1 = cv2.norm((left_thumb1.x,left_thumb1.y,left_thumb1.z),(right_index1.x,right_index1.y,right_index1.z))
+                    finger_distance1/=shoulder_distance1
+                    # 어깨 사이의 각도 계산
+                    #shoulder_angle = calculate_angle(left_shoulder_coords, head_coords, right_shoulder_coords)
+
+                    # 각도에 따라 고개의 상태 표시
+                    """if shoulder_angle > 20:  # 예제의 임계값
+                        cv2.putText(frame, "Head Down", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+                    else:
+                        cv2.putText(frame, "Head Up", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)"""
+
+                    # 각도를 화면에 표시
+                    #cv2.putText(frame, f"Angle: {shoulder_angle:.2f} degrees", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+                    hna = head_nod_algorithm(left_ear,right_ear)#(left_shoulder_coords,right_shoulder_coords,head_coords)
+                    cv2.putText(frame, f"LR diff : {hna[1]:.2f}", (50, 140), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+                    ph = [0,int(5*hna[0])]
                     
-                    if distance/distance1 > 1.4:  # 이 값은 실험을 통해 조절할 수 있습니다.
-                        current_time = time.time()
+                    for i2,p2 in enumerate(ph):
+                        phLocal[i2] += p2
                         
-                        if current_time - last_capture_time >= capture_interval:
-                            sharedPicture.value = 1
-                            play_mp3(mp3_file)
-                            # 내가 원하는 이미지와 함께 촬영
-                            #cv2.imwrite('captured_desired_image.jpg', desired_image)
-                            last_capture_time = current_time
+                    PitchHeading[:] = phLocal
+                        
+                # 손 감지 수행
+                resultsHand = hands.process(rgb_frame)
+                # 손 키포인트를 그리기 위한 코드
+                if resultsHand.multi_hand_landmarks:
+                    for landmarks in resultsHand.multi_hand_landmarks:
+                        mp_drawing.draw_landmarks(frame, landmarks, mp_hands.HAND_CONNECTIONS)
+
+                        # 검지와 엄지 각도 계산
+                        index_finger = landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
+                        thumb = landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP]
+                        indexMCP = landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_MCP]
+                        distance = cv2.norm(
+                            (index_finger.x, index_finger.y,index_finger.z),
+                            (thumb.x, thumb.y,thumb.z)
+                        )
+                        distance1 =cv2.norm(
+                            (index_finger.x, index_finger.y,index_finger.z),
+                            (indexMCP.x,indexMCP.y,indexMCP.z)
+                        )
+                        if results.pose_landmarks:
+                            distance2 = finger_distance1
+                        else:
+                            distance2 = 1
+                        print(distance2)
+                        if distance/distance1 > 1.4 and distance2 <0.3:  # 이 값은 실험을 통해 조절할 수 있습니다.
+                            current_time = time.time()
                             
-                            
-
-                    #print(Picture)
-                            
-                    
-
-            results = pose.process(frame)
-
-            if results.pose_landmarks:
-                # 어깨(landmark 12, 11)와 머리(landmark 30)의 좌표 획득
-                left_shoulder = results.pose_landmarks.landmark[12]
-                right_shoulder = results.pose_landmarks.landmark[11]
-                head = results.pose_landmarks.landmark[30]
-
-                # 각 랜드마크의 좌표를 화면 좌표로 변환
-                left_shoulder_coords = (int(left_shoulder.x * frame.shape[1]), int(left_shoulder.y * frame.shape[0]))
-                right_shoulder_coords = (int(right_shoulder.x * frame.shape[1]), int(right_shoulder.y * frame.shape[0]))
-                head_coords = (int(head.x * frame.shape[1]), int(head.y * frame.shape[0]))
-
-                # 어깨 사이의 각도 계산
-                shoulder_angle = calculate_angle(left_shoulder_coords, head_coords, right_shoulder_coords)
-
-                # 각도에 따라 고개의 상태 표시
-                if shoulder_angle > 20:  # 예제의 임계값
-                    cv2.putText(frame, "Head Down", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-                else:
-                    cv2.putText(frame, "Head Up", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-
-                # 각도를 화면에 표시
-                cv2.putText(frame, f"Angle: {shoulder_angle:.2f} degrees", (50, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
-                ph = [0,0]
-                PitchHeading[:] = ph
+                            if current_time - last_capture_time >= capture_interval:
+                                sharedPicture.value = 1
+                                play_mp3(mp3_file)
+                                # 내가 원하는 이미지와 함께 촬영
+                                #cv2.imwrite('captured_desired_image.jpg', desired_image)
+                                last_capture_time = current_time
+                                
+                        #print(Picture)
                 
-            
 
-            cv2.imshow('Webcam', frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+                cv2.imshow('Webcam', frame)
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+            except:
+                time.sleep(0.5)
+                print(traceback.format_exc())
 
     cap.release()
     cv2.destroyAllWindows()
